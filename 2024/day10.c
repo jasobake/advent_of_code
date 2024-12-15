fa6c7438d6bd3820a111668de0a1acfe1575ba15
jasobake <j@sonbaker.com>
2024-12-14 21:07:06 -0800

    cleaned up day 10

--- 2024/day10.c
+++ 2024/day10.c
@@ -7,32 +7,30 @@
 
 #define SIZE 41
 
-bool checked[SIZE][SIZE] = { false };
-
-size_t check(size_t i, size_t n, char t, size_t p) {
-    if (checked[i][n]) return 0;
+size_t check(size_t i, size_t n, char t, bool (*checked)[SIZE]) {
+    if (checked[i][n]) return 0; // disable this check for part 2 solution
     checked[i][n] = true;
 
-    size_t total = p;
+    size_t total = 0;
     char new_target = t + 1;
     if (new_target == '9' + 1) {
         return 1;
     }
     //top
     if (i > 0 && data[i-1][n] == new_target) {
-        total += (check(i-1, n, new_target, p));
+        total += (check(i-1, n, new_target, checked));
     }
     //bottom
     if (i < (SIZE - 1) && data[i+1][n] == new_target) {
-        total += (check(i+1, n, new_target, p));
+        total += (check(i+1, n, new_target, checked));
     }
     //left
     if (n > 0 && data[i][n-1] == new_target) {
-        total += (check(i, n-1, new_target, p));
+        total += (check(i, n-1, new_target, checked));
     }
     //right
     if (n < (SIZE - 1) && data[i][n+1] == new_target) {
-        total += (check(i, n+1, new_target, p));
+        total += (check(i, n+1, new_target, checked));
     }
 
     return total;
@@ -40,12 +38,14 @@ size_t check(size_t i, size_t n, char t, size_t p) {
 
 int main() {
     size_t c = 0;
+    bool checked[SIZE][SIZE] = { false };
+
     for (size_t i = 0; i < SIZE; i++)
         for (size_t n = 0; n < SIZE; n++) {
             if (data[i][n] == '0') {
-                size_t score = check(i, n, '0', 0);
+                memset(checked, false, sizeof(checked));
+                size_t score = check(i, n, '0', checked);
                 c += score;
-                memset(checked, 0, sizeof(checked));
             }
         }
     
